4a6f261157045aa00dc2560dd5e6f1bd995c8683
Chidi Nwaneto <nwanetochidiebere@gmail.com>
2020-12-24 15:08:44 +0000

    Update train.py

--- train.py
+++ train.py
@@ -10,17 +10,17 @@ import pandas as pd
 from azureml.core.run import Run
 from azureml.data.dataset_factory import TabularDatasetFactory
 
-# TODO: Create TabularDataset using TabularDatasetFactory
-# Data is located at:
-# "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
+from azureml.core import Dataset, Datastore
+from azureml.data.datapath import DataPath
 
-ds = ### YOUR CODE HERE ###
+web_path ='https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv'
+
+ds = Dataset.Tabular.from_delimited_files(path=web_path)
 
 x, y = clean_data(ds)
 
 # TODO: Split data into train and test sets.
-
-### YOUR CODE HERE ###a
+sklearn.model_selection.train_test_split(x, y)
 
 run = Run.get_context()
 
@@ -49,7 +49,6 @@ def clean_data(data):
     x_df["poutcome"] = x_df.poutcome.apply(lambda s: 1 if s == "success" else 0)
 
     y_df = x_df.pop("y").apply(lambda s: 1 if s == "yes" else 0)
-    
 
 def main():
     # Add arguments to script
